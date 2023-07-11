95ec9ea89bbbd29d38dddfa0f186c6e8ca177d2c
JuniorWSP <juniorwender@yahoo.com.br>
2023-07-11 18:44:06 -0300

    adicionando onclick e link ao JS

--- arquivos/zodiaco.js
+++ arquivos/zodiaco.js
@@ -4,3 +4,9 @@ function colocaFoto(imagem) {
 function retiraFoto() {
     document.getElementById("local").src = "../imagens/transparente.webp"
 }
+function sites(links) {
+    window.location.href = links;
+}
+function home() {
+    window.location.href = "zodiaco.html"
+}
